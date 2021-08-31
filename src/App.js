c2fd2d14d58c3c76c7787e09b17eeaa82a7929c5
Ian French <ianfrench33@gmail.com>
2021-08-31 18:56:44 -0400

    made three buttons to potential decide from, each very rough. npm install @material-ui/core. probably shouldve branched off

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Banner from "./images/Mister_Js_Logo.jpg";
+import BeerPic from "./images/beer_photo.jpg";
+import "./App.css";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   NavLink,
 } from "react-router-dom";
+import { Button } from "@material-ui/core";
 
 function App() {
   return (
@@ -20,16 +23,57 @@ function App() {
         ></img>
       </div>
       <Router>
-        <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center", height: "30vh", marginTop: "6vh"}}>
-        <NavLink to="/inventory">
-          <div style={{ display:"flex", width:"25vw", height: "30vh", alignItems:"center", justifyContent: "center" }}><p>VIEW OUR INVENTORY</p></div>
-        </NavLink>
-        <NavLink to="/contact">
-          <div style={{ display:"flex", width:"25vw", height: "30vh", alignItems:"center", justifyContent: "center", }}><p style={{ textAlign: "center"}}>LEAVE A COMMENT OR MAKE A REQUEST</p></div>
-        </NavLink>
-        <NavLink to="/about">
-          <div style={{ display:"flex", width:"25vw", height: "30vh", alignItems:"center", justifyContent: "center" }}><p>ABOUT</p></div>
-        </NavLink>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            height: "30vh",
+            marginTop: "6vh",
+            marginBottom: "6vh",
+          }}
+        >
+          <NavLink to="/inventory" style={{ textDecoration: "none" }}>
+            <div className="home-nav-div">
+              <div className="home-nav-button">
+                <p style={{ color: "white" }}>VIEW OUR INVENTORY</p>
+              </div>
+            </div>
+          </NavLink>
+
+          <Button variant="contained">
+            <NavLink to="/contact">
+              <div
+                style={{
+                  display: "flex",
+                  width: "25vw",
+                  height: "30vh",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <p style={{ textAlign: "center" }}>
+                  LEAVE A COMMENT OR MAKE A REQUEST
+                </p>
+              </div>
+            </NavLink>
+          </Button>
+
+          <NavLink to="/about">
+            <div
+              style={{
+                display: "flex",
+                width: "25vw",
+                height: "30vh",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Button variant="outlined">
+                <p>ABOUT</p>
+              </Button>
+            </div>
+          </NavLink>
         </div>
         <Switch>
           <Route path="/inventory" />
